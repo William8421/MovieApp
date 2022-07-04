e971d90eb31533863f1e5d1801ac5bd3a2284c0e
william <william.mallak1@gmail.com>
2022-07-04 15:44:41 +0200

    deleted console log

--- movie-app/src/routes/Movies.js
+++ movie-app/src/routes/Movies.js
@@ -18,7 +18,6 @@ export default function Movies() {
         const {data} = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`)
         setContent(data.results)
         setNumOfPages(data.total_pages)
-        console.log(data);
     };
 
 
