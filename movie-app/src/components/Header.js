944c42e88a75355665c383d9553de453344bb94a
william <william.mallak1@gmail.com>
2022-07-08 19:44:46 +0200

    changed navlink to normal link

--- movie-app/src/components/Header.js
+++ movie-app/src/components/Header.js
@@ -1,11 +1,9 @@
 import React, {useState} from 'react';
-// import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import WhatshotIcon from '@mui/icons-material/Whatshot';
 import MovieIcon from '@mui/icons-material/Movie';
 import SeriesIcon from '@mui/icons-material/Tv';
 import SearchIcon from '@mui/icons-material/Search';
-import { NavLink } from 'react-router-dom';
 
 
 export default function SimpleBottomNavigation() {
@@ -28,12 +26,6 @@ export default function SimpleBottomNavigation() {
       }
     })
   }
-  
-  function blurHandler(e){
-    console.log('close');
-    setMenu('off', e.target.value)
-    setBurger('close', e.target.value)
-  }
 
   return (
     <div>
@@ -45,15 +37,15 @@ export default function SimpleBottomNavigation() {
           <div className={`bar bottom ${burger}`}></div>
         </div>
         
-        <NavLink className='nameBox' to={'/'}>
+        <a className='nameBox' href={'/'}>
         <img src="/movie-icon.svg" alt=""/>
         <span className="appName">Movie App</span>
-        </NavLink>
-        <div className={`navbar ${menu}`} onBlur={blurHandler}>
-        <NavLink className="navIcon" to={'/'} ><BottomNavigationAction className='icon' icon={<WhatshotIcon />} />Trending</NavLink>
-        <NavLink className="navIcon" to={'/movies'} ><BottomNavigationAction className='icon' icon={<MovieIcon />} /><div>Movies</div></NavLink>
-        <NavLink className="navIcon" to={'/series'} ><BottomNavigationAction className='icon'  icon={<SeriesIcon />} /><div>Series</div></NavLink>
-        <NavLink className="navIcon" to={'/search'} ><BottomNavigationAction className='icon'  icon={<SearchIcon />} /><div>Search</div></NavLink>
+        </a>
+        <div className={`navbar ${menu}`}>
+        <a className="navIcon" href={'/'} ><BottomNavigationAction className='icon' icon={<WhatshotIcon />} />Trending</a>
+        <a className="navIcon" href={'/movies'} ><BottomNavigationAction className='icon' icon={<MovieIcon />} /><div>Movies</div></a>
+        <a className="navIcon" href={'/series'} ><BottomNavigationAction className='icon'  icon={<SeriesIcon />} /><div>Series</div></a>
+        <a className="navIcon" href={'/search'} ><BottomNavigationAction className='icon'  icon={<SearchIcon />} /><div>Search</div></a>
         </div>
       </div>
       </div>
